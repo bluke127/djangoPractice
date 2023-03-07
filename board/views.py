9e22660c78c8c1db16ba373bfa520b103efeccfa
dongwookang <dnehdrkd123@naver.com>
2023-03-07 20:18:21 +0900

    [fix]regist 성공

--- board/views.py
+++ board/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from .models import Board
 from .forms import RegistForm
 # Create your views here.
@@ -7,6 +8,12 @@ def index(request):
     context = {'board_list':board_list}
     return render(request, 'board/index.html', context)
 def regist(request):
-    form = RegistForm()
+    if request.method== 'POST' :
+        form = RegistForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('board:index')
+    else:
+        form = RegistForm()
     context = {'form':form}
     return render(request,'board/regist_form.html',context)
